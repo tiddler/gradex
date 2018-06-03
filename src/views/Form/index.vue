af8260468946307257de6be2f640e6c9875eb2f2
Jaredream <zhihaowang619@gmail.com>
2018-06-03 22:33:23 +0800

    Added the last two tabs in the Crossing Modification Tab

--- src/views/Form/index.vue
+++ src/views/Form/index.vue
@@ -73,6 +73,8 @@
           <v-tab href="#tab-2">Crossing General</v-tab>
           <v-tab href="#tab-3">Crossing Geometry</v-tab>
           <v-tab href="#tab-4">Crossing Traffic</v-tab>
+          <v-tab href="#tab-5">Crossing TSB</v-tab>
+          <v-tab href="#tab-6">Crossing Info Sharing</v-tab>
         </v-tabs>
       </v-toolbar>
       <v-tabs-items v-model="inventory_tabs">
@@ -848,6 +850,126 @@
             </v-data-table>            
           </v-card>
         </v-tab-item>
+        <v-tab-item :id="'tab-5'">
+          <v-card>
+            <v-card-title>
+              <div>
+                <h3 class="title-text">Vehicle / Pedestrain Accidents</h3>
+              </div>
+            </v-card-title>
+            <v-data-table
+              :headers="headers_accident"
+              :items="vpAccident"
+              class="elevation-1"
+              :pagination.sync="pagination_accident"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{ props.item.occurNum }}</td>
+                <td class="text-xs-right">{{ props.item.occurYear }}</td>
+                <td class="text-xs-right">{{ props.item.occurDate }}</td>
+                <td class="text-xs-right">{{ props.item.occurTime }}</td>
+                <td class="text-xs-right">{{ props.item.occurType }}</td>
+                <td class="text-xs-right">{{ props.item.occurIncType }}</td>
+                <td class="text-xs-right">{{ props.item.operator1 }}</td>
+                <td class="text-xs-right">{{ props.item.operator2 }}</td>
+                <td class="text-xs-right">{{ props.item.train1 }}</td>
+                <td class="text-xs-right">{{ props.item.train2 }}</td>
+                <td class="text-xs-right">{{ props.item.involvedTrain }}</td>
+                <td class="text-xs-right">{{ props.item.involvedCars }}</td>
+                <td class="text-xs-right">{{ props.item.involvedDCars }}</td>
+                <td class="text-xs-right">{{ props.item.nearestTown }}</td>
+                <td class="text-xs-right">{{ props.item.trackType }}</td>
+                <td class="text-xs-right">{{ props.item.derail }}</td>
+                <td class="text-xs-right">{{ props.item.taInjuries }}</td>
+                <td class="text-xs-right">{{ props.item.taFatal }}</td>
+                <td class="text-xs-right">{{ props.item.taSerious }}</td>
+                <td class="text-xs-right">{{ props.item.taMinor }}</td>
+                <td class="text-xs-right">{{ props.item.tfEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tfPass }}</td>
+                <td class="text-xs-right">{{ props.item.tfMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tfMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tfTrespass }}</td>
+                <td class="text-xs-right">{{ props.item.tfOther }}</td>
+                <td class="text-xs-right">{{ props.item.tsEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tsPass }}</td>
+                <td class="text-xs-right">{{ props.item.tsMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tsMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tsTraspass }}</td>
+                <td class="text-xs-right">{{ props.item.tsOthers }}</td>
+                <td class="text-xs-right">{{ props.item.tmEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tmPass }}</td>
+                <td class="text-xs-right">{{ props.item.tmMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tmMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tmTrespass }}</td>
+                <td class="text-xs-right">{{ props.item.tmOthers }}</td>
+                <td class="text-xs-right">{{ props.item.cause1 }}</td>
+                <td class="text-xs-right">{{ props.item.cause2 }}</td>
+                <td class="text-xs-right">{{ props.item.cause3 }}</td>
+                <td class="text-xs-right">{{ props.item.occuSummary }}</td>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item :id="'tab-6'">
+          <v-card>
+            <v-card-title>
+              <div>
+                <h3 class="title-text">Information Shring Details</h3>
+              </div>
+            </v-card-title>
+            <v-data-table
+              :headers="headers_"
+              :items="vpAccident"
+              class="elevation-1"
+              :pagination.sync="pagination_accident"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{ props.item.occurNum }}</td>
+                <td class="text-xs-right">{{ props.item.occurYear }}</td>
+                <td class="text-xs-right">{{ props.item.occurDate }}</td>
+                <td class="text-xs-right">{{ props.item.occurTime }}</td>
+                <td class="text-xs-right">{{ props.item.occurType }}</td>
+                <td class="text-xs-right">{{ props.item.occurIncType }}</td>
+                <td class="text-xs-right">{{ props.item.operator1 }}</td>
+                <td class="text-xs-right">{{ props.item.operator2 }}</td>
+                <td class="text-xs-right">{{ props.item.train1 }}</td>
+                <td class="text-xs-right">{{ props.item.train2 }}</td>
+                <td class="text-xs-right">{{ props.item.involvedTrain }}</td>
+                <td class="text-xs-right">{{ props.item.involvedCars }}</td>
+                <td class="text-xs-right">{{ props.item.involvedDCars }}</td>
+                <td class="text-xs-right">{{ props.item.nearestTown }}</td>
+                <td class="text-xs-right">{{ props.item.trackType }}</td>
+                <td class="text-xs-right">{{ props.item.derail }}</td>
+                <td class="text-xs-right">{{ props.item.taInjuries }}</td>
+                <td class="text-xs-right">{{ props.item.taFatal }}</td>
+                <td class="text-xs-right">{{ props.item.taSerious }}</td>
+                <td class="text-xs-right">{{ props.item.taMinor }}</td>
+                <td class="text-xs-right">{{ props.item.tfEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tfPass }}</td>
+                <td class="text-xs-right">{{ props.item.tfMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tfMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tfTrespass }}</td>
+                <td class="text-xs-right">{{ props.item.tfOther }}</td>
+                <td class="text-xs-right">{{ props.item.tsEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tsPass }}</td>
+                <td class="text-xs-right">{{ props.item.tsMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tsMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tsTraspass }}</td>
+                <td class="text-xs-right">{{ props.item.tsOthers }}</td>
+                <td class="text-xs-right">{{ props.item.tmEmp }}</td>
+                <td class="text-xs-right">{{ props.item.tmPass }}</td>
+                <td class="text-xs-right">{{ props.item.tmMoto }}</td>
+                <td class="text-xs-right">{{ props.item.tmMotoPass }}</td>
+                <td class="text-xs-right">{{ props.item.tmTrespass }}</td>
+                <td class="text-xs-right">{{ props.item.tmOthers }}</td>
+                <td class="text-xs-right">{{ props.item.cause1 }}</td>
+                <td class="text-xs-right">{{ props.item.cause2 }}</td>
+                <td class="text-xs-right">{{ props.item.cause3 }}</td>
+                <td class="text-xs-right">{{ props.item.occuSummary }}</td>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-tab-item>
       </v-tabs-items>
     </v-card>
     </v-dialog>
@@ -1331,6 +1453,54 @@ export default {
           CPPerdestrain: 85.00,
           CPVehicle: 85.00
         }
+      ],
+      headers_accident: [
+        { text: 'Occurence #', align: 'center', value: 'occurNum' },
+        { text: 'Occurence Year', align: 'center', value: 'occurYear' },
+        { text: 'Occurence Date', align: 'center', value: 'occurDate' },
+        { text: 'Occurence Time', align: 'center', value: 'occurTime' },
+        { text: 'Occurence Type', align: 'center', value: 'occurType' },
+        { text: 'Occurence Acc. Inc. Type', align: 'center', value: 'occurIncType' },
+        { text: 'Train Operator #1', align: 'center', value: 'operator1' },
+        { text: 'Train Operator #2', align: 'center', value: 'operator2' },
+        { text: 'Train Type #1', align: 'center', value: 'train1' },
+        { text: 'Train Type #2', align: 'center', value: 'train2' },
+        { text: 'Involved # of Trains', align: 'center', value: 'involvedTrain' },
+        { text: 'Involved # of Cars', align: 'center', value: 'involvedCars' },
+        { text: 'Involved # of Dangerous Goods Cars', align: 'center', value: 'involvedDCars' },
+        { text: 'Nearest Town / City', align: 'center', value: 'nearestTown' },
+        { text: 'Track Type', align: 'center', value: 'trackType' },
+        { text: 'Derailment', align: 'center', value: 'derail' },
+        { text: 'Total All Injuries', align: 'center', value: 'taInjuries' },
+        { text: 'Total All Fatal', align: 'center', value: 'taFatal' },
+        { text: 'Total All Serious', align: 'center', value: 'taSerious' },
+        { text: 'Total All Minor', align: 'center', value: 'taMinor' },
+        { text: 'Total Fatal Employees', align: 'center', value: 'tfEmp' },
+        { text: 'Total Fatal Passengers', align: 'center', value: 'tfPass' },
+        { text: 'Total Fatal Motorists', align: 'center', value: 'tfMoto' },
+        { text: 'Total Fatal Motorist Passengers', align: 'center', value: 'tfMotoPass' },
+        { text: 'Total Fatal Trespassers', align: 'center', value: 'tfTrespass' },
+        { text: 'Total Fatal Others', align: 'center', value: 'tfOther' },
+        { text: 'Total Serious Employees', align: 'center', value: 'tsEmp' },
+        { text: 'Total Serious Passengers', align: 'center', value: 'tsPass' },
+        { text: 'Total Serious Motorists', align: 'center', value: 'tsMoto' },
+        { text: 'Total Serious Motorist Passengers', align: 'center', value: 'tsMotoPass' },
+        { text: 'Total Serious Trespassers', align: 'center', value: 'tsTraspass' },
+        { text: 'Total Serious Others', align: 'center', value: 'tsOthers' },
+        { text: 'Total Minor Employees', align: 'center', value: 'tmEmp' },
+        { text: 'Total Minor Passengers', align: 'center', value: 'tmPass' },
+        { text: 'Total Minor Motorists', align: 'center', value: 'tmMoto' },
+        { text: 'Total Minor Motorist Passengers', align: 'center', value: 'tmMotoPass' },
+        { text: 'Total Minor Trespassers', align: 'center', value: 'tmTrespass' },
+        { text: 'Total Minor Others', align: 'center', value: 'tmOthers' },
+        { text: 'Primary Cause Level 1', align: 'center', value: 'cause1' },
+        { text: 'Primary Cause Level 2', align: 'center', value: 'cause2' },
+        { text: 'Primary Cause Level 3', align: 'center', value: 'cause3' },
+        { text: 'Occurence Summary', align: 'center', value: 'occuSummary' }
+      ],
+      pagination_accident: {},
+      vpAccident: [
+
       ]
     }
   },
